78a684030a61b4c580960dfbc9a814b86d3f8970
Liwenzhao524 <202322080714@std.uestc.edu.cn>
2025-01-21 20:31:14 +0800

    LeftHand IK

--- Assets/Scripts/WeaponVisualController.cs
+++ Assets/Scripts/WeaponVisualController.cs
@@ -6,21 +6,30 @@ public class WeaponVisualController : MonoBehaviour
 {
     [SerializeField] Transform[] _weapons;
 
+    Transform _currentWeapon;
+
+    [SerializeField] Transform _leftHandIKTarget;
 
     private void Start()
     {
-        SwitchOffWeapons();
+        SwitchOnWeapon(_weapons[0]);
     }
 
     private void Update()
     {
-        // SwitchWeapon With 1234
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SwitchOnWeapon(_weapons[0]);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchOnWeapon(_weapons[1]);
     }
 
-    void SwichOnWeapon(Transform weapon)
+    void SwitchOnWeapon(Transform weapon)
     {
         SwitchOffWeapons();
         weapon.gameObject.SetActive(true);
+        _currentWeapon = weapon;
+
+        SetLeftHandIK();
     }
 
     void SwitchOffWeapons()
@@ -29,5 +38,13 @@ public class WeaponVisualController : MonoBehaviour
         {
             _weapons[i].gameObject.SetActive(false);
         }
+    }
+
+    void SetLeftHandIK()
+    {
+        Transform target = _currentWeapon.GetComponentInChildren<LeftHandIKManage>().transform;
+
+        _leftHandIKTarget.localPosition = target.localPosition;
+        _leftHandIKTarget.localRotation = target.localRotation;
     }
 }
